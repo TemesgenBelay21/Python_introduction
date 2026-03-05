1070f38f7f2b503ac0d8649b55f937039f6d20e7
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-05 18:59:50 +0100

    this are some of the methods in string

---
+++ phase1/string_methods.py
@@ -0,0 +1,10 @@
+
+course = "python programming"
+
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("jro" in course)
