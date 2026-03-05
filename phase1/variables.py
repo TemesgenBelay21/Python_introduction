5a893e548f8da5bd6b3b51edb3ea8987c8fd8978
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-05 14:10:37 +0100

    this are variables with operators

---
+++ phase1/variables.py
@@ -0,0 +1,13 @@
+
+num1 = float(input("please enter the first number"))
+
+num2 = float(input("please enter the second number"))
+
+sum = num1 + num2
+diffrence = num1 - num2
+multiplication = num1 * num2
+
+
+print(sum)
+print(diffrence)
+print(multiplication)
