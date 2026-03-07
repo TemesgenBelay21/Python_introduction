db986b13f2d3323e8b2b20d953277d9ac0744247
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-03-07 09:26:06 +0100

    ternary operator in python

---
+++ phase1/ternary_operator.py
@@ -0,0 +1,15 @@
+
+# age = 18
+# if age >= 18:
+#     message = "elligible"
+# else:
+#     message = "not elligible"
+
+# print(message)
+
+#  ternary operator for the above code
+
+age = 18
+message = "elligible" if age >= 18 else "not elligible "
+
+print(message)
